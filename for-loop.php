98cb9c55a94892a11b42307c768aceecb864062e
unknown <your@email.example>
2025-10-30 09:36:41 +0200

    third commit

---
+++ for-loop.php
@@ -0,0 +1,10 @@
+
+<?php
+
+for ($i = 7; $i <= 16; $i++) {
+    echo $i;
+}
+   //Izveido 'for' ciklu, kas izvada veselus skaitļus no 7 līdz 16;
+?>
+
+
--- variables.php
+++ variables.php
@@ -1,10 +1,10 @@
 
 <?php
 
-$groupName = 'PT2025';
-$size = 25;
-$isActive = false;
-define("EULR", 2.718);
+$groupName = 'PT2025'; //Izveidojiet 'string' tipa mainīgo "groupName", kurā tiek glabāts tava kursa nosaukums;
+$size = 25; //Izveidojiet 'number' tipa mainīgo "size", kurā tiek glabāts studentu skaits grupā;
+$isActive = false; //Izveidojiet 'boolean' tipa mainīgo "isActive", kura vērtība NAV patiesa;
+define("EULR", 2.718); //Izveidojiet konstanti "EULER", kura vērtība ir 2.718;
 
 
 ?>
